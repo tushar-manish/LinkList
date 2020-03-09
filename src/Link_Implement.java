e20318883e576d3d935ef7c0f06307ce23abd710
tushar-manish <tushargupta9412@gmail.com>
2020-03-09 07:17:54 -1000

    Implementation of various function on Linklist

--- src/Link_Implement.java
+++ src/Link_Implement.java
@@ -1,2 +1,26 @@
 public class Link_Implement {
+    Node head;
+
+    Link_Implement(){
+        head = null;
+    }
+
+    public void insert(int data){
+        Node ns = new Node(data);
+        if (isempty()){
+            head = ns;
+        }
+        else{
+            Node tmp = head;
+            while(tmp.next!=null) tmp = tmp.next;
+            tmp.next = ns;
+        }
+    }
+
+
+    private boolean isempty() {
+        return head==null;
+    }
+
+
 }
